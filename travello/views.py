d02d39a11b61cb9fc873ab63cba812fe9f98b8df
md.yasin <yasinbuetcse14@gmail.com>
2020-12-12 21:43:41 +0600

    destination added

--- travello/views.py
+++ travello/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import Destination
+
+
 # Create your views here.
 
 
 def index(request):
-
     dest1 = Destination()
     dest1.name = 'Mumbai'
     dest1.desc = 'The City That Never Sleeps'
@@ -26,3 +27,7 @@ def index(request):
     dests = [dest1, dest2, dest3]
 
     return render(request, "index.html", {'dests': dests})
+
+
+def destination(request):
+    return render(request, "destination.html")
