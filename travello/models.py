a415c0db568e9d4afae443d2c1568cce4499736f
md.yasin <yasinbuetcse14@gmail.com>
2020-12-15 21:50:11 +0600

    js added

--- travello/models.py
+++ travello/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Destination:
     id: int
     name: str
--- travello/views.py
+++ travello/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from .models import Destination
 
-
 # Create your views here.
-
 
 def index(request):
     dest1 = Destination()
